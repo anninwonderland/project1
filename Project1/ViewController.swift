34772d0ffe744c2e181c4d313b8a60d960e3ba13
@anninwonderland <anninwonderland@yandex.ru>
2019-04-23 23:37:48 +0300

    First project on Swift: image viewer

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath! //might not have resource path
+        let items = try! fm.contentsOfDirectory(atPath: path) //try to read
+        
+        for item in items {
+            if item.hasPrefix("0") {
+                //load the picture
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count //as many cells as we have pictures
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row] //do this if only there some text inside, otherwise do nothing
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
 }
 
